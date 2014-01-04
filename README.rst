0eadf68ccd02b50ee6c39c3df73a9e76900ea855
Yotam Bar-On <tudmotu@gmail.com>
2014-01-04 18:26:00 +0200

    New version with a couple of fixes. Also: - Modified 'description' in metadata to better explain what the extension actually does. - Added version - Added URL to ego page in README - Re-indented some files

--- README.rst
+++ README.rst
@@ -7,6 +7,9 @@ This simple extension implements two small features:
 #. When using the mouse-scroll over the volume indicator, the volume slider appears and displays the appropriate level.
 #. Clicking on the volume indicator using the mouse middle button will toggle output mute.
 
+Extension page on e.g.o:
+https://extensions.gnome.org/extension/759/better-volume-indicator/
+
 Installation
 ----------------
 
--- convenience.js
+++ convenience.js
@@ -1,20 +1,20 @@
 // Print objects... why no dev tools
 function dbPrintObj (name, obj, recurse, _indent) {
-	let prefix = '';
-	let indent = typeof _indent === 'number' ? _indent : 0;
-	for (let i = 0; i < indent; i++) {
-		prefix += '    ';
-	}
+    let prefix = '';
+    let indent = typeof _indent === 'number' ? _indent : 0;
+    for (let i = 0; i < indent; i++) {
+        prefix += '    ';
+    }
 
-	log(prefix + '--------------');
-	log(prefix + name);
-	log(prefix + '--------------');
-	for (let k in obj) {
-		if (typeof obj[k] === 'object' && recurse) {
-			dbPrintObj(name + '::' + k, obj[k], true, indent + 1);
-		}
-		else {
-			log(prefix + k, typeof obj[k] === 'function' ? '[Func]' : obj[k]);
-		}
-	}
+    log(prefix + '--------------');
+    log(prefix + name);
+    log(prefix + '--------------');
+    for (let k in obj) {
+        if (typeof obj[k] === 'object' && recurse) {
+            dbPrintObj(name + '::' + k, obj[k], true, indent + 1);
+        }
+        else {
+            log(prefix + k, typeof obj[k] === 'function' ? '[Func]' : obj[k]);
+        }
+    }
 }
